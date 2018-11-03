3d83ef7af9375561ad7e591620865e1130066b3b
vagrant <vagrant@localhost.localdomain>
2018-11-03 15:25:10 +0000

    Implement basic login

--- Gemfile
+++ Gemfile
@@ -5,6 +5,10 @@ ruby '2.5.1'
 gem 'bcrypt'
 gem 'carrierwave'
 
+gem 'jquery-rails'
+
+gem 'jquery-ui-rails'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.1'
 # Use sqlite3 as the database for Active Record
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
-  # user root
   resources :users
+  # users
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
+  # sessions
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end
